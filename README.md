25d359ffb85103c62d95c419334d329c6467bcd6
anthonyliu0417 <liuyunlong0417@gmail.com>
2022-09-29 20:12:55 +0800

    final commit

--- README.md
+++ README.md
@@ -1,7 +1,6 @@
-# BIPT-Pass-Gen
-北石化出入证生成工具
+# (deprecated)BIPT-Pass-Gen
+【过时的】北石化出入证生成工具
 
 ### iOS系统的cookie不支持中文字符，本程序使用了encodeURIComponent进行编码
 
 头像编码为Base64，然后使用localstorage传递到新的页面。
-web前端知识全是自学的，可能不太规范，如果有问题还请大家多多指正
--- input.js
+++ input.js
@@ -1,6 +1,6 @@
 var queren=confirm("声明：\n点击“确定”并继续使用代表您已阅读并同意以下内容\n1. 本项目仅供学习和研究web及网络知识使用，请勿将其用于任何非法用途否则后果自负，本站不承担一切责任。\n2. 源代码以MIT License于GitHub开源，有兴趣的话可以点击页面上方的GitHub链接进入我的Repositories查看。\n3. 由于本人技术水平还有待提高，如果大家有发现不规范的地方或者bug请在github中提交issue一起交流进步，感谢大家理解和帮助。\n4. 本工具一切操作全部通过JavaScript完成，所有数据均在前端由浏览器处理，后端服务器不储存不处理任何信息。\n5. 页面间的数据传递是通过Cookies完成的，如果浏览器没有打开Cookies则信息可能不会被正确显示出来。\n6. 由于Cookies大小被限制在4k，所以照片使用了sessionstorage实现传递，大小限制各浏览器不同基本在5M左右，如果你需要自定义照片，请确认手机浏览器内核支持sessionstorage，否则照片可能不会被正确显示。如果不支持请通过升级系统解决或使用默认头像。\n7. 只要浏览器或微信不关闭，cookies就不会被清除，所以如果微信或者浏览器一直在后台没关，就可以随时打开xdgclub.com/crzsl查看上一次生成的页面，直到下一次生成将它覆盖。\n最后，运营网站不易，购买域名及服务器都需要资金，如果您觉得本站内容有帮助的话欢迎通过页面下方二维码进行捐助，谢谢。");
 if (queren==false){
-    window.location.href="https://xdgclub.com";
+    window.location.href="/";
 }
 document.getElementById("btn").onclick=function(){
     localStorage.removeItem("photo");
@@ -30,12 +30,12 @@ document.getElementById("btn").onclick=function(){
     else{
         if (sex[0].checked){
             var boypic=new Image();
-            boypic.src="https://xdgclub.com/resources/boys.jpg";
+            boypic.src="crz/images/boys.jpg";
             pic=boypic.src;
         }
         if (sex[1].checked){
             var girlpic=new Image();
-            girlpic.src="https://xdgclub.com/resources/girls.jpg";
+            girlpic.src="crz/images/girls.jpg";
             pic=girlpic.src;
         }
     }    
